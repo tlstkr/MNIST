f96f964861d73e7a1d7df7cf937728e0bf8f702e
tlstkr <tlstkr@icloud.com>
2021-06-29 12:38:40 +0300

    Update ipynb.py

--- ipynb.py
+++ ipynb.py
@@ -5,16 +5,7 @@
 
 
 import tensorflow as tf
-import ssl
-
-try:
-    _create_unverified_https_context = ssl._create_unverified_context
-except AttributeError:
-    # Legacy Python that doesn't verify HTTPS certificates by default
-    pass
-else:
-    # Handle target environment that doesn't support HTTPS verification
-    ssl._create_default_https_context = _create_unverified_https_context
+import numpy as np
 
 from keras.datasets import mnist
 
@@ -29,25 +20,14 @@ model.add(tf.keras.layers.Dense(128, activation = tf.nn.relu))
 model.add(tf.keras.layers.Dense(10, activation = tf.nn.softmax))
 
 
-
 model.compile(optimizer = 'adam',
              loss = 'sparse_categorical_crossentropy',
              metrics = ['accuracy'])
 
 model.fit(x_train, y_train, epochs = 3)
 
-
-# (dataset of 28x28 images of hand-written digits from 0 to 9)
-
-# In[ ]:
-
-
 val_loss, val_acc = model.evaluate(x_test, y_test)
 print(val_loss, val_acc)
-
-
-# In[77]:
-
 
 import matplotlib.pyplot as plt
 
@@ -56,47 +36,20 @@ plt.show()
 
 print(x_train[0])
 
-
-# In[69]:
-
-
 model.save('epic_num_reader.model')
-
-
-# In[70]:
-
 
 new_model = tf.keras.models.load_model('epic_num_reader.model')
 
-
-# In[71]:
-
-
 predictions = new_model.predict([x_test])
-
-
-# In[73]:
-
 
 print(predictions)
 
-
-# In[75]:
-
-
-import numpy as np
-
 print(np.argmax(predictions[0]))
-
-
-# In[79]:
-
 
 plt.imshow(x_test[0])
 plt.show()
 
 
-# In[ ]:
 
 
 
